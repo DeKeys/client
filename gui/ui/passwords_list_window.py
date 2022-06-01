c8d0a344051653b8c17c6f5d7181eb8998e54b8f
SkullMag <rybalko.oleg.123@mail.ru>
2022-06-01 22:43:27 +0300

    Refactored code and added functionality for showing icon

---
+++ gui/ui/passwords_list_window.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout
+
+from gui.src.widgets.passwords_list_header import PasswordsListHeader
+from gui.src.widgets.passwords_list_widget import PasswordsListWidget
+
+
+class Ui_PasswordsListWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(700, 500)
+
+        self.setWindowTitle("DeKeys")
+
+        # Create central widget and layout
+        self.centralWidget = QWidget()
+        self.gridLayout = QGridLayout()
+
+        # Create header and widget for listing passwords
+        self.header = PasswordsListHeader(self)
+        self.passwords_list = PasswordsListWidget(self)
+
+        # Add widgets to the layout
+        self.gridLayout.addWidget(self.header, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.passwords_list, 1, 0, 1, 1)
+
+        # Set margin to zero
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+
+        # Add layout to central widget and set new central widget
+        self.centralWidget.setLayout(self.gridLayout)
+        self.setCentralWidget(self.centralWidget)
+
