6cf13534fd600deb33609b5b034a8f8b6c501ada
SkullMag <rybalko.oleg.123@mail.ru>
2022-06-01 22:42:47 +0300

    Moved window to another file

--- GUI_client.py
+++ GUI_client.py
@@ -1,32 +1,9 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton, QLabel
 from PyQt5.QtGui import QFont
 
-from src.widgets.passwords_list_header import PasswordsListHeader
-from src.widgets.passwords_list_widget import PasswordsListWidget
+from gui.src.windows.passwords_list_window import PasswordsListWindow
 
 import sys
-
-
-class PasswordsListWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("DeKeys")
-
-        self.centralWidget = QWidget()
-        self.gridLayout = QGridLayout()
-
-        self.header = PasswordsListHeader(self)
-        self.passwords_list = PasswordsListWidget(self)
-
-        self.gridLayout.addWidget(self.header, 0, 0, 1, 1)
-        self.gridLayout.addWidget(self.passwords_list, 1, 0, 1, 1)
-
-        self.centralWidget.setLayout(self.gridLayout)
-        self.setCentralWidget(self.centralWidget)
-
-    def complete_get_passwords(self, reply):
-        print(reply)
 
 
 if __name__ == "__main__":
