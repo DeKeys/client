bd8aa197d6fd12cde436ba52b3e3061ce2f0fee9
SkullMag <rybalko.oleg.123@mail.ru>
2022-06-02 04:36:01 +0300

    Get passwords from backend

--- gui/src/windows/passwords_list_window.py
+++ gui/src/windows/passwords_list_window.py
@@ -1,7 +1,70 @@
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtCore import QUrl, QByteArray
+
 from gui.ui.passwords_list_window import Ui_PasswordsListWindow
+from gui.src.widgets.password_list_widget_item import PasswordListWidgetItem
+from gui.constants import private_key, public_key, pub_key_string
+from gui.utils import generate_verification
+
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+import json
+import secrets
+from binascii import unhexlify
 
 
 class PasswordsListWindow(Ui_PasswordsListWindow):
     def __init__(self):
         super().__init__()
 
+        # Create network manager
+        self.networkManager = QNetworkAccessManager()
+        self.networkManager.finished.connect(self.finishedGettingPasswords)
+
+        # Create requests for getting passwords
+        request = QNetworkRequest(QUrl("http://217.28.228.66:8000/api/get_passwords"))
+        data, signature = generate_verification()
+        self.networkManager.sendCustomRequest(
+            request, 
+            QByteArray("GET".encode("utf-8")), 
+            QByteArray(json.dumps({
+                "public_key": pub_key_string, 
+                "verification_string": data, 
+                "signature": signature
+            }).encode("utf-8"))
+        )
+
+        self.passwords = []
+
+    def finishedGettingPasswords(self, reply):
+        encrypted_passwords = json.loads(json.loads(bytes(reply.readAll()).decode("utf-8")))
+        res = []
+        for pwd in encrypted_passwords["passwords"]:
+            pwd["service"] = private_key.decrypt(
+                unhexlify(pwd["service"]),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA512()),
+                    algorithm=hashes.SHA512(),
+                    label=None
+                )
+            ).decode("utf-8")
+            pwd["login"] = private_key.decrypt(
+                unhexlify(pwd["login"]),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA512()),
+                    algorithm=hashes.SHA512(),
+                    label=None
+                )
+            ).decode("utf-8")
+            pwd["password"] = private_key.decrypt(
+                unhexlify(pwd["password"]),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA512()),
+                    algorithm=hashes.SHA512(),
+                    label=None
+                )
+            ).decode("utf-8")
+            res.append(pwd)
+            self.passwordsList.addItem(PasswordListWidgetItem(pwd["service"]))
+        self.passwords = res
